155e8996b642ac754a4d935e70785176499ecf2e
“chenaokun” <chenaokun1990@FT106-122-M1.local>
2022-03-30 10:05:09 -0400

    Modified exection script

--- run_classification.sh
+++ run_classification.sh
@@ -5,36 +5,35 @@
 # we assume the batch data is in ./data/batch_data
 # you should expect there are several directories named as batch_* with a test.tsv file in each of them
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
+data_dir= /home/chenaokun1990/datasets/n2c2_2022_cls
+nmd=./new_modelzw
+pof=./gatortron_syn_n2c2_pred.txt
+log=./log_gatortron_n2c2.txt
 
-model_type=megatron
-data_format_mode=0
-max_seq_length=512
-data_file_has_header=True
-tag_for_non_relation=NonRel
-mode=bin # binary tags pos/neg
-binary_type_mapping_file=./data/mappings.json
-relation_extraction_model=./model
-data_dir=./data/batch_data
-pred_dir=./data/predict_batch
-brat_entity_dir=./data/results_from_NER
-final_brat_output_with_NER_RE=./data/final
-log=./log.txt
-
-python ./src/batch_prediction.py \
-  --model_type $model_type \
-  --data_format_mode $data_format_mode \
-  --new_model_dir $relation_extraction_model \
-  --predict_output_dir $pred_dir \
-  --max_seq_length $max_seq_length \
-  --data_file_header $data_file_has_header \
-  --do_lower_case \
-  --eval_batch_size 32 \
-  --log_file $log \
-  --log_lvl i \
-  --num_core 4 \
-  --non_relation_label $tag_for_non_relation \
-  --classification_mode $mode \
-  --type_map $binary_type_mapping_file \
-  --entity_data_dir $brat_entity_dir \
-  --brat_result_output_dir $final_brat_output_with_NER_RE
+# NOTE: we have more options available, you can check our wiki for more information
+python ./src/relation_extraction.py \
+		--model_type gatortron \
+		--data_format_mode 0 \
+		--classification_scheme 1 \
+		--pretrained_model /home/alexgre/projects/transformer_pretrained_models/345m_uf_syn_pubmed_mimic_wiki_fullcased50k_megatronv22_release \
+		--data_dir $data_dir \
+		--new_model_dir $nmd \
+		--predict_output_file $pof \
+		--overwrite_model_dir \
+		--seed 13 \
+		--max_seq_length 256 \
+		--cache_data \
+		--do_train \
+		--do_eval \
+		--do_lower_case \
+		--train_batch_size 4 \
+		--eval_batch_size 4 \
+		--learning_rate 1e-5 \
+		--num_train_epochs 20 \
+		--gradient_accumulation_steps 1 \
+		--do_warmup \
+		--warmup_ratio 0.1 \
+		--weight_decay 0 \
+		--max_num_checkpoints 1 \
+		--log_file $log \
